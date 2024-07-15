bb57320a369919e8ef674aef6231a8ea5454bf61
dawitmanukian <davit.manukyan.d@tumo.org>
2024-07-15 16:50:49 +0400

    updates

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,10 +5,12 @@ import { ReactComponent as Logo } from "../../images/Logo.svg";
 import { ReactComponent as LogoM } from "../../images/logo_mob.svg";
 import { ReactComponent as Menu } from "../../images/menu.svg";
 import MobileMenu from "../mobile_menu/MobileMenu";
+import { useLocation } from "react-router-dom";
 
 const Header = ({onOpen}) => {
 
   const [showMenu, setShowMenu] = useState(false)
+  const location = useLocation();
 
   return (
     <header className={classes.header}>
@@ -19,9 +21,9 @@ const Header = ({onOpen}) => {
         <Link to={"/"} style={{ margin: "0px" }}>
           <Logo />
         </Link>
-        <Link to={"/contacts"}>Contacts</Link>
-        <Link to={"/stores"}>Stores</Link>
-        <Link to={"/faq"}>FAQ</Link>
+        <Link to={"/contacts"} className={classes.lnk} style={location.pathname === "/contacts" ? { borderBottom: "2px solid #fff" } : null}>Contacts</Link>
+        <Link to={"/stores"} className={classes.lnk} style={location.pathname === "/stores" ? { borderBottom: "2px solid #fff" } : null}>Stores</Link>
+        <Link to={"/faq"} className={classes.lnk} style={location.pathname === "/faq" ? { borderBottom: "2px solid #fff" } : null}>FAQ</Link>
       </div>
       <Link to={"/order"}>
         <button className={classes.submit}>SUBMIT ORDER</button>
