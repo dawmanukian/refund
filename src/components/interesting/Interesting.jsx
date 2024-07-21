f34b842e83f1f158a694b5bb023d53ca718fdfec
dawitmanukian <davit.manukyan.d@tumo.org>
2024-07-21 23:01:42 +0400

    update

--- src/components/interesting/Interesting.jsx
+++ src/components/interesting/Interesting.jsx
@@ -30,33 +30,29 @@ const Interesting = () => {
         <div className={classes.box}>
           <img src={Team} />
           <div className={classes.texts}>
-            <p className={classes.header}>Thousands of Reviews</p>
+            <p className={classes.header}>Team of Lawyers</p>
             <span>
-              We have completed more than 1000 orders and left more than 300
-              customers satisfied. Not many can boast of such an achievement.
-              Feel free to work with us!
+              We collaborate with a team of corporate lawyers who have years of
+              experience in the refunding industry, ensuring the highest
+              possible success rate when it comes to getting your funds back.
             </span>
           </div>
         </div>
         <div className={classes.box}>
           <div className={classes.texts}>
-            <p className={classes.header}>Thousands of Reviews</p>
+            <p className={classes.header}>Reliable service</p>
             <span>
-              We have completed more than 1000 orders and left more than 300
-              customers satisfied. Not many can boast of such an achievement.
-              Feel free to work with us!
+            RefCat has been the most efficient, reliable, and reputable refunding service on the market for years. We will get your refund done without a doubt.Our customer support is available to answer over Telegram anytime, to any inquiries you may have in your mind.
             </span>
           </div>
-          <img src={Reliable} style={{width: "591px", height: "368px"}}/>
+          <img src={Reliable} style={{ width: "591px", height: "368px" }} />
         </div>
         <div className={classes.box}>
           <img src={Results} />
           <div className={classes.texts}>
-            <p className={classes.header}>Thousands of Reviews</p>
+            <p className={classes.header}>Guaranteed Results</p>
             <span>
-              We have completed more than 1000 orders and left more than 300
-              customers satisfied. Not many can boast of such an achievement.
-              Feel free to work with us!
+            Even though words are nice, we focus on providing maximized results in as short timeframe as possible. Get in touch to learn more!
             </span>
           </div>
         </div>
