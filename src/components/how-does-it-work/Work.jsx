bfc9e3cb6b4b8f0ed4ed4989135f1d25817e5bb4
dawitmanukian <davit.manukyan.d@tumo.org>
2024-07-22 17:40:51 +0400

    updates

--- src/components/how-does-it-work/Work.jsx
+++ src/components/how-does-it-work/Work.jsx
@@ -11,6 +11,68 @@ const Work = () => {
   return (
     <section className={classes.section}>
       <p className={classes.header}>How Does it Work?</p>
+      <div className={classes.boxs_mob}>
+        <div className={classes.box}>
+          <div className={classes.nm}>
+            <div className={classes.num}>
+              <span>1</span>
+            </div>
+          </div>
+          <p className={classes.box_header}>Contact Us</p>
+          <div className={classes.bx_data}>
+            <ContactUs />
+            <p className={classes.box_body}>
+              Contact us on telegram to recommand you the best store at the
+              moment and place you order.
+            </p>
+          </div>
+        </div>
+        <div className={classes.box}>
+          <div className={classes.nm}>
+            <div className={classes.num}>
+              <span>2</span>
+            </div>
+          </div>
+          <p className={classes.box_header}>Order & receive</p>
+          <div className={classes.bx_data}>
+            <Order />
+            <p className={classes.box_body}>
+            Order from the preferred website and wait for the item to be
+            delivered to you.
+            </p>
+          </div>
+        </div>
+        <div className={classes.box}>
+          <div className={classes.nm}>
+            <div className={classes.num}>
+              <span>3</span>
+            </div>
+          </div>
+          <p className={classes.box_header}> Countact us again to get the refund!</p>
+          <div className={classes.bx_data}>
+            <Refund />
+            <p className={classes.box_body}>
+            After you receive the products, message us with the details to
+            process a refund for you.
+            </p>
+          </div>
+        </div>
+        <div className={classes.box}>
+          <div className={classes.nm}>
+            <div className={classes.num}>
+              <span>4</span>
+            </div>
+          </div>
+          <p className={classes.box_header}>Payment</p>
+          <div className={classes.bx_data}>
+            <Payment />
+            <p className={classes.box_body}>
+            After the refund is confirmed, you have the pay for the refund-fee
+            and leave us a feedback :)
+            </p>
+          </div>
+        </div>
+      </div>
       <div className={classes.boxs}>
         <div className={classes.box}>
           <div className={classes.num}>
@@ -72,7 +134,7 @@ const Work = () => {
             <div className={classes.r}></div>
           </div>
           <p className={classes.mb_p}>and more than 1000 completed orders</p>
-          <Link to={'/order'}>
+          <Link to={"/order"}>
             <button className={classes.mb_submit}>SUBMIT ORDER</button>
           </Link>
         </div>
